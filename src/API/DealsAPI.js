03dca91e503ec7b01ffda973394d942dd822e95c
unknown <bmika@icloud.com>
2020-11-18 16:43:32 -0500

    Added API mockup for create deals.

--- src/API/DealsAPI.js
+++ src/API/DealsAPI.js
@@ -4,7 +4,38 @@ const DEALS_KEY = "deals_data";
 export function postCreateDeal(postData, onSuccessCallbackFunc, onFailureCallbackFunc) {
     console.log("Starting postCreateDeal...");
 
-    
+    // GET the TEXT data from localStorage.
+    let textData = localStorage.getItem(DEALS_KEY);
+
+    // If TEXT data is null then:
+    if (textData === null) {
+        // a. Create an empty ARRAY
+        const emptyArray = [];
+
+        // b. Convert the ARRAY to TEXT
+        const emptyArrayString = JSON.stringify(emptyArray);
+
+        // c. Save the TEXT to localStorage
+        localStorage.setItem(DEALS_KEY, emptyArrayString);
+
+        // d. Set the TEXT to be empty
+        textData = emptyArrayString;
+    }
+
+    // Convert the TEXT data into an ARRAY
+    const arrayData = JSON.parse(textData);
+
+    // Push the `postData` into our ARRAY
+    arrayData.push(postData);
+
+    // Convert the ARRAY to TEXT data
+    textData = JSON.stringify(arrayData);
+
+    // Save the TEXT data to localStorage.
+    localStorage.setItem(DEALS_KEY, textData);
+
+    const responseData = { message: "Added" };
+    onSuccessCallbackFunc(responseData);
 }
 
 export function getListDeals(onSuccessCallbackFunc, onFailureCallbackFunc) {
